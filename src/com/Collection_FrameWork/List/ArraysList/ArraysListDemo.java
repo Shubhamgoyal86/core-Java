138fc8c219a6e9ad994d441fd6ca4969315eec80
Shubhamgoyal86 <tsshubhamyt2022@gmail.com>
2022-09-10 20:49:59 +0530

    day 8

--- src/com/Collection_FrameWork/List/ArraysList/ArraysListDemo.java
+++ src/com/Collection_FrameWork/List/ArraysList/ArraysListDemo.java
@@ -1,4 +1,12 @@
 package com.Collection_FrameWork.List.ArraysList;
 
+
 public class ArraysListDemo {
+    public static void main(String[] args) {
+        //We can see How to creATE AN aRRAYLIST using the Arraylist() constructor......
+        // add new elements to an Arraylist using the add() method......
+
+
+    }
+
 }
